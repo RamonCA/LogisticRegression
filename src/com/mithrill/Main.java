d7aed4949c7e6ee41e078b80ab966d9212c93d65
Ramon Calderon <za17011424@zapopan.tecmm.edu.mx>
2021-11-26 17:57:12 -0600

    Data set and dynamic algorithm

--- src/com/mithrill/Main.java
+++ src/com/mithrill/Main.java
@@ -7,7 +7,7 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         logRegression test = new logRegression();
-        test.weighCalc();
+        System.out.println(Arrays.toString(test.weighCalc()));
         List<Data> dataSet = com.mithrill.dataSet.readData("arrayD2.txt");
 
         /*for (Data d : dataSet){
--- src/com/mithrill/logRegression.java
+++ src/com/mithrill/logRegression.java
@@ -13,7 +13,7 @@ public class logRegression {
 
     private final double[][] input_x = new double [size][2];
     private final double[][] output_y = new double [size][1];
-    double[] weights = new double [size];
+    double[] weights = new double [2];
 
     private void getData (){
         for (int i = 0; i < size; i++){
@@ -21,39 +21,41 @@ public class logRegression {
             input_x[i][0] = 1;
             input_x[i][1] = aux.getInput_x();
             output_y[i][0] = aux.getOutput_y();
-            weights[i] = 0.0;
+
         }
+        Arrays.fill(weights, 0.0);
     }
 
-    public void weighCalc() {
+    public double [] weighCalc() {
         getData();
-        for (int i = 0; i < 100; i++) {
+        for (int i = 0; i < 10000; i++) {
             double[] tempWeights = new double[weights.length];
             double sum = 0;
             weightSum(tempWeights, sum);
-            System.arraycopy(tempWeights, 0, weights, 0, input_x.length);
-            System.out.println(weights[0] + " " + weights[1]);
+            System.arraycopy(tempWeights, 0, weights, 0, input_x[0].length);
+            //System.out.println(weights[0] + " " + weights[1]);
         }
+        return weights;
     }
 
     private double [] weightSum (double[] tempWeights, double sum){
-        for (int j = 0; j < input_x.length; j++) {
-            for (int k = 0; k < input_x[0].length; k++) {
+
+        for (int j = 0; j < input_x[0].length; j++) {
+            for (int k = 0; k < input_x.length; k++) {
                 sum += ((1 / ( 1 + Math.exp( - sigmoidCalculation(k))))
-                        - output_y[k][0] ) * input_x[j][k];
+                        - output_y[k][0] ) * input_x[k][j];
             }
-            double alpha = 0.1;
+            double alpha = 0.00001;
             tempWeights[j] = weights[j] - alpha * sum;
             sum = 0;
         }
-
         return tempWeights;
     }
 
     private Double sigmoidCalculation(int index) {
         double init = weights[0];
         for (int i = 1; i < weights.length; i++) {
-            init += weights[i] * input_x[i][index];
+            init += weights[i] * input_x[index][i];
         }
         return init;
     }
